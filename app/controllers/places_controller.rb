253239734a9b0fdf2c62960d4e042968fe4a84f1
nora m <nora.ma@kellogg.northwestern.edu>
2024-05-11 21:42:34 +0000

    Individual place created

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,5 +4,22 @@ class PlacesController < ApplicationController
     @places = Place.all
   end
 
+  def show
+    @place = Place.find (params["id"])
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new (place_params)
+    if @place.save
+      @place = Place.all
+    else
+      @place = Place.new
+    end
+  end
+
 end
 
