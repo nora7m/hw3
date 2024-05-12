58ec84d01bb4b0906e5886172b544cf3788167a5
nora m <nora.ma@kellogg.northwestern.edu>
2024-05-12 04:49:43 +0000

    created new place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -17,12 +17,18 @@ class PlacesController < ApplicationController
   def create
     @place = Place.new(place_params)
     if @place.save
-      redirect_to "/places"  # Redirect to the list of all places
+      puts "Place was successfully created."
+      redirect_to places_path
     else
-      redirect_to "/places/new"  # Redirect back to the new form
+      puts "Failed to create place."
+      redirect_to new_place_path
     end
   end
 
+  def place_params
+    params.require(:place).permit(:name)
+  end
+  
 end
 
 
